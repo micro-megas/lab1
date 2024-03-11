49c54375414d192ac20d8017012e0219f8fc4827
micro-megas <micro-megas@yandex.ru>
2024-03-11 05:34:05 +0300

    Initial

--- flaskapp/start.sh
+++ flaskapp/start.sh
@@ -1,6 +1,10 @@
-/home/appveyor/.local/bin/waitress-serve --call 'flaskr:create_app' & APP_PID=$!
-sleep 5
+echo start server
+/home/appveyor/.local/bin/waitress-serve --call 'wsgi:create_app' & APP_PID=$!
 echo $APP_PID
+sleep 5
+echo start client
+python3 client.py
+sleep 5
 kill -TERM $APP_PID
 echo process waitress-serve kills
 exit 0
